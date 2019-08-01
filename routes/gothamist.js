d91da928199c18051e34079d0f4fe2358d9f7f44
Brian Whitton <me@brianwhitton.com>
2019-08-01 10:54:03 -0400

    move in-module defined vars to bottom of list

--- routes/gothamist.js
+++ routes/gothamist.js
@@ -3,9 +3,9 @@ const rp = require('request-promise-native');
 const jsdom = require('jsdom');
 
 
-const GOTH_HOST = process.env.GOTHAMIST_HOST;
 const { JSDOM } = jsdom;
 const router = express.Router();
+const GOTH_HOST = process.env.GOTHAMIST_HOST;
 
 router.get('/:section/articles/:slug', async (req, res, next) => {
   const { section, slug } = req.params;
