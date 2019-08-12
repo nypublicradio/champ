0b94f9b5c1d6067d2e83516df57bed0cfd200bcb
Brian Whitton <me@brianwhitton.com>
2019-08-12 16:01:03 -0400

    install and configure sentry
    specify the environment. use `dev` as a fallback in case local devs want to send to sentry withouth polluting the live environments

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const Sentry = require('@sentry/node');
 
 const gothamist = require('../routes/gothamist');
 
 
 const app = express();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  environment: process.env.NODE_ENV || 'dev',
+});
 
 // express error handlers require an arity of 4
 // eslint-disable-next-line no-unused-vars
@@ -16,8 +22,10 @@ const errorHandler = (err, req, res, next) => {
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
+app.use(Sentry.Handlers.requestHandler());
 app.use('/gothamist', gothamist);
 
+app.use(Sentry.Handlers.errorHandler());
 app.use(errorHandler);
 
 module.exports = app;
