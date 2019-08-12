c1f8c6be8843531710338d901c867ee28c35c7d1
Brian Whitton <me@brianwhitton.com>
2019-08-12 09:55:20 -0400

    calm the linter

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const gothamist = require('../routes/gothamist');
 
 const app = express();
 
+// express error handlers require an arity of 4
+// eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   res.status(err.statusCode);
   res.render('error', {layout: false});
