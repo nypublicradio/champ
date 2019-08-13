4117ea0cc6382bac6451ffbb3e3cb092cccbbf58
Brian Whitton <bwhitton@wnyc.org>
2019-08-13 14:37:31 -0400

    ampify images in article header and body

--- routes/gothamist.js
+++ routes/gothamist.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const rp = require('request-promise-native');
 const jsdom = require('jsdom');
+const get = require('just-safe-get');
+
+const {
+  amplify,
+  ampImg
+} = require('../lib/amp');
 
 
 const { JSDOM } = jsdom;
 const router = express.Router();
 const GOTH_HOST = process.env.GOTHAMIST_HOST;
+
+const HEADER_SELECTOR = '.c-article__header';
+const BODY_SELECTOR = '.c-article__body';
 
 router.get(`/:section/:slug`, async (req, res, next) => {
   const { section, slug } = req.params;
@@ -19,12 +28,16 @@ router.get(`/:section/:slug`, async (req, res, next) => {
   }
 
   const { document } = (new JSDOM(html)).window;
-  const header = document.querySelector('.c-article__header');
+  const header = document.querySelector(HEADER_SELECTOR);
+  const body = document.querySelector(BODY_SELECTOR);
+
+  amplify(header, 'img', ampImg);
+  amplify(body, 'img', ampImg);
 
   const locals = {
     title: document.title,
-    header: header ? header.outerHTML : null,
-    body: header ? header.nextElementSibling.outerHTML : null,
+    header: get(header, 'outerHTML'),
+    body: get(body, 'outerHTML'),
   };
 
   try {
