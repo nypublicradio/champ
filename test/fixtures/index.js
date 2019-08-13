ba76ad85ff4b4121d8a9a212a157a87d8c4c39d4
Brian Whitton <me@brianwhitton.com>
2019-08-13 00:06:42 -0400

    article body fixture

---
+++ test/fixtures/index.js
@@ -0,0 +1,249 @@
+const ARTICLE_BODY = `
+<article class="c-article l-container u-spacing--double">
+  <header id="ember776" class="c-article__header u-spacing--and-half l-container ember-view">
+    <div class="l-container l-container--12col u-spacing--and-half">
+      <div id="ember778" class="o-breadcrumbs ember-view"><a href="/news" id="ember780" class="o-breadcrumb ember-view"> News
+        </a></div>
+
+      <h1 id="ember781" class="c-article__headline o-headline ember-view">
+        Fixture Article Title
+      </h1>
+    </div>
+
+    <div id="ember782" class="l-container l-container--10col ember-view">
+      <div class="c-article__meta u-spacing--half u-font--secondary-style u-font--xxs">
+        <span id="ember783" class="o-byline ember-view"> By
+          <a href="/author/wnyc" id="ember784" class="u-has-accent u-has-accent--neutral ember-view"> WNYC WNYC
+          </a>
+          &nbsp;and&nbsp;<a href="/author/cindy-rodriguez" id="ember785" class="u-has-accent u-has-accent--neutral ember-view"> Cindy Rodriguez
+          </a>
+        </span>
+        <div class="c-article__meta-group">
+          <div id="ember786" class="o-published-date ember-view"><span class="o-published-date__time">
+              <time datetime="2019-08-02T11:36-04:00">
+                Aug. 2, 2019 11:36 a.m.
+              </time>
+            </span>
+          </div>
+          •
+          <a href="#comments" class="u-has-accent u-has-accent--secondary">
+            6 Comments
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div id="ember787" class="c-article__lead ember-view">
+      <div class="l-container l-container--10col">
+        <div class="c-article-header__left c-article__share u-border-accent u-border-accent--right">
+
+          <div>
+
+
+
+            <!---->
+
+
+
+          </div>
+
+          <div class="c-article-header__center">
+            <figure role="group" id="ember821" class="o-figure ember-view c-lead">
+              <div id="ember822" class="o-figure__image ember-view">
+                <picture id="ember823" class="o-picture ember-view">
+                  <!---->
+                  <img src="https://gothamist.imgix.net/attachments/arts_jen/IMG_2836.jpg?crop=faces&amp;fit=crop&amp;auto=compress,format&amp;fm=jpg&amp;w=640&amp;q=90" itemprop="image" alt="RIP old green.">
+                </picture>
+              </div>
+
+              <figcaption id="ember824" class="o-caption ember-view"> <span class="o-icon o-icon--dashed-arrow u-icon--s">
+                  <svg class="arrow-dashed-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                    <title>Dashed Arrow</title>
+                    <polygon points="18.74 3.12 29.96 14 18.74 24.88 18.74 16.72 15 16.72 15 11.28 18.74 11.28 18.74 3.12"></polygon>
+                    <rect x="8.2" y="11.28" width="5.44" height="5.44"></rect>
+                    <rect x="4.12" y="11.28" width="2.72" height="5.44"></rect>
+                    <rect x="0.04" y="11.28" width="2.72" height="5.44"></rect>
+                  </svg>
+
+
+                </span>
+                <div class="o-caption__text">
+                  <span>
+                    RIP old green.
+                  </span>
+
+                  <span class="o-credit">
+                    Jen Carlson / Gothamist
+                  </span>
+
+                </div>
+
+              </figcaption>
+            </figure>
+
+          </div>
+        </div>
+
+      </div>
+  </header>
+
+  <div>
+
+    <div id="ember801" class="c-article__body o-rte-text u-spacing l-container--content ember-view u-space--zero--top">
+      <p><span class="mt-enclosure mt-enclosure-image" style="display: inline;"></span></p>
+      <p></p>
+      <p>The NYPD officer accused of using an illegal chokehold that ended Eric Garner’s life has been found guilty of misconduct in a police disciplinary trial—the final stages of a case that has defined police-community relations in New York City
+        under Mayor Bill de Blasio.</p>
+      <p>The administrative judge overseeing the case, Deputy Commissioner Rosemary Maldonado, has recommended that Officer Daniel Pantaleo be fired, according to a source familiar with the decision. Judge Maldonado found him guilty of using an
+        illegal chokehold, but not guilty of intentionally obstructing Garner’s breathing, the source said.</p>
+      <p>The attorneys on both sides now have two weeks to comment on Judge Rosemarie Maldonado’s decision. Then her decision and the attorneys’ comments go to Police Commissioner James O’Neill, who has final say on whether to accept the
+        recommendation or decide on a different outcome.</p>
+      <p>Mayor de Blasio is expected to hold a City Hall press conference at 1 p.m. to comment on the case.</p>
+      <div id="708749" class="">
+        <div id="ember837" class="ad-tag-wide ember-view">
+          <div id="ember838" class="ember-view">
+            <div class="break-margins">
+              <div class="flex-wrapper">
+                <div id="ad_ember838"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p>Judge Maldonado’s decision and recommendation was supposed to <a href="https://gothamist.com/2019/05/20/pantaleo_trial_eric_garner_death.php">remain confidential</a> under a controversial state law known as 50-A, which protects an officer’s
+        personnel records. In a recent court case, the New York Civil Liberties Union argued that the documents generated during a disciplinary proceeding should remain public as long as an officer’s name was redacted. The state’s highest court <a
+          href="https://law.justia.com/cases/new-york/court-of-appeals/2018/133.html" target="_blank" rel="noopener">disagreed</a> and they lost the case. </p>
+      <p><span class="mt-enclosure mt-enclosure-image" style="display: inline;"></span></p>
+      <div class="image-none"><img alt="8219carr.jpg" src="https://gothamist.com/attachments/nyc_chrisrobbins/8219carr.jpg" width="640" height="427"><br><i> Gwen Carr joins a "young people" rally in Federal Plaza to protest the decision by federal
+          prosecutors not to bring civil rights charges against Daniel Pantaleo (Bebeto Matthews/AP/Shutterstock) </i></div><br><br>
+      <p>
+        The disciplinary trial started in May, when a senior forensic pathologist testified that a chokehold and chest compression triggered an asthma attack, cardiac arrest and ultimately, Garner’s death. Dr. Florina Persechino called it a “lethal
+        cascade” of events. </p>
+      <p></p>
+      <p><em>This story is being updated as we receive more information.</em></p>
+
+    </div>
+
+  </div>
+
+  <div id="ember836" class="ember-view"></div>
+
+  <div class="l-container--content u-section-spacing">
+    <div id="ember805" class="c-article__footer u-section-spacing ember-view">
+      <div class="c-article__tags">
+
+        <div id="ember806" class="o-tags ember-view">
+          <a href="/tags/daniel%20pantaleo" id="ember808" class="o-tag ember-view"> #daniel pantaleo
+          </a>
+
+
+          <a href="/tags/eric%20garner" id="ember810" class="o-tag ember-view"> #eric garner
+          </a>
+
+
+          <a href="/tags/nypd" id="ember812" class="o-tag ember-view"> #nypd
+          </a>
+
+
+          <a href="/tags/staten%20island" id="ember814" class="o-tag ember-view"> #staten island
+          </a>
+
+        </div>
+
+      </div>
+
+      <div class="u-section-spacing">
+        <div class="c-article__contact">
+
+
+          <div id="ember815" class="c-contact-tout o-rte-text u-reversed-out o-3d-heading ember-view">
+            <p><strong>Do you know the scoop?</strong> &nbsp; <a href="#comments">Comment</a> below or <a href="mailto:tips@gothamist.com" target="_blank" rel="noopener">Send us a Tip</a></p>
+
+          </div>
+
+        </div>
+
+        <div class="c-article__donate">
+
+          <div id="ember816" class="c-donate-tout ember-view">
+            <div class="c-donate-tout__inner">
+              <button class="   c-donate-tout__close" role="button" type="button">
+
+                <span class="o-icon u-icon--xs o-icon--close">
+                  <svg class="close-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                    <title>Close</title>
+                    <path d="M4.43.76A2.6,2.6,0,0,0,.76,4.43L11.33,15,.76,25.57a2.6,2.6,0,1,0,3.67,3.67L15,18.67,25.57,29.24a2.6,2.6,0,1,0,3.67-3.67L18.67,15,29.24,4.43A2.6,2.6,0,1,0,25.57.76L15,11.33Z" fill-rule="evenodd"></path>
+                  </svg>
+
+                </span>
+
+              </button>
+
+              <div class="c-donate-tout__graphic">
+                <svg class="pointing-finger-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 133">
+                  <title>Finger Pointing</title>
+                  <path class="hand"
+                    d="M108.44,82.56l-22-34.27a2.26,2.26,0,0,0-.4-.47v-.73C86.09,39.32,78.61,33,69.42,33c-7,0-13,3.7-15.49,8.91a18.36,18.36,0,0,0-13.36-1.44c-6.84,2-9.53,8.08-10.51,11.42a28.38,28.38,0,0,0-13.38,4.25C10.47,60,10,67.92,10.28,72.2A17.44,17.44,0,0,0,2.9,74.74c-5.39,3.51-1.76,13.55-1,15.55l6.87,17A21.05,21.05,0,0,0,21.12,118a31.72,31.72,0,0,0,10.32,2.08,14.78,14.78,0,0,0,11.85-5.41,18.22,18.22,0,0,0,2,.12h.15c-3.58,5.2-3.38,10.86-1.27,14.52A6.63,6.63,0,0,0,49.74,133a5.06,5.06,0,0,0,1.48-.22c2.87-.87,20.58-2,31.08-2.46a3,3,0,0,0,.65-.11c.93-.29,9.6-3.47,25.61-27.63C114.72,93.28,111.29,85.56,108.44,82.56Zm-39-44.45c6.31,0,11.45,4,11.45,9V58c0,5.18-5.26,9.72-11.27,9.72-5.46,0-10.48-3.74-11.45-8.19.08-1.9,1.69-4.7,4.38-6.23,3-1.74,6.71-1.51,10.62.65a2.65,2.65,0,0,0,3.17-.47,2.62,2.62,0,0,0,.89-.27,2.5,2.5,0,0,0,1-3.4c-3.08-5.55-7.25-8.83-11.73-9.25a11.47,11.47,0,0,0-6.32,1.31,12.63,12.63,0,0,1,9.28-3.74ZM58,50.09c1.88-2.26,4.78-4.86,8-4.57a7.43,7.43,0,0,1,4,1.8A14.42,14.42,0,0,0,59.9,48.87,13.62,13.62,0,0,0,58,50.22Zm-18.5,58.47a37.33,37.33,0,0,1-11.31-8.09L20.47,83.56A2.64,2.64,0,0,0,17,82.26a2.54,2.54,0,0,0-1.32,3.38l7.85,17.23a2.11,2.11,0,0,0,.33.54c.18.22,4.45,5.47,13.34,9.76l.13,0c-3.31,2.36-7.81,2.33-14.46,0a15.79,15.79,0,0,1-9.25-7.83l-6.86-17c-1.6-4-2.09-8.71-1-9.44A13.66,13.66,0,0,1,13,77.17a2.56,2.56,0,0,0,2.11-.86,2.48,2.48,0,0,0,.62-2.16c0-.1-1.86-10.22,3.74-13.7A22.34,22.34,0,0,1,31.89,57a2.58,2.58,0,0,0,2.83-2.19c0-.08,1.15-7.67,7.31-9.43a13.47,13.47,0,0,1,10.72,1.75V58A12.6,12.6,0,0,0,53,60.11s0,.06,0,.1a14.65,14.65,0,0,0,5,8.25l5.4,16.45a2.53,2.53,0,0,0,1.37,1.53c6.42,3,12.63,6.84,13.26,8.16.3,1.62-1.46,5.12-2.83,7.14a5.65,5.65,0,0,1-1.65.52c-.52,0-1.7.09-3.3.28a56.35,56.35,0,0,1-11.06-1.29C45.31,98.49,43.93,90,43.89,89.69a2.71,2.71,0,0,0-.09-.41L36.43,64.14a2.62,2.62,0,0,0-3.22-1.75,2.55,2.55,0,0,0-1.79,3.16l7.32,25c.29,1.65,2.44,10.51,15.14,14.6a10,10,0,0,1-4.78,3.91C44.53,110.74,39.47,108.56,39.47,108.56ZM63,126.36h0c-3.79.28-7.37.6-9.94.94a4.73,4.73,0,0,1,.92-2.5c1.49-1.84,4.76-2.16,7.39-2.41l.91-.12C62.66,122.62,63,124.42,63,126.36ZM104.18,99.8h0C90.66,120.21,82.9,124.6,81.6,125.24c-1.92.09-7.45.36-13.43.75,0-2.18-.33-5.43-2.22-7.31a5.33,5.33,0,0,0-4.33-1.45l-.81.08c-3.22.31-8.09.78-11,4.31a9.24,9.24,0,0,0-1.76,3.58c-.38-1.62-.79-6,4.1-10.24,5.78-5,13.94-6.75,18.43-7.32,5.92-.1,8-1.78,8.66-2.76h0a26.45,26.45,0,0,0,3.14-6c2.07-4.24,2.31-9.84.54-13.47a2.63,2.63,0,0,0-3.48-1.2,2.53,2.53,0,0,0-1.23,3.4,5.7,5.7,0,0,1,.24.62,68.32,68.32,0,0,0-10.53-6L64.61,72.08a17.57,17.57,0,0,0,5,.73C78.54,72.81,86.09,66,86.09,58v-.7l18.07,28.21a2.54,2.54,0,0,0,.36.45C104.58,86,109.82,91.3,104.18,99.8Z">
+                  </path>
+                  <path class="middle-knuckle" d="M38.09,95.26l-3.93,2.53a3.22,3.22,0,0,0-1,3.65A2.09,2.09,0,0,0,35,103a1.61,1.61,0,0,0,.87-.27l3.93-2.52a3.19,3.19,0,0,0,1-3.65c-.48-1.37-1.7-1.94-2.71-1.3Z"></path>
+                  <path class="right-knuckle" d="M51,95a1.83,1.83,0,0,0,1.14-.42l4-3.26a2.68,2.68,0,0,0,.51-3.3,1.83,1.83,0,0,0-2.79-.6l-4,3.26a2.67,2.67,0,0,0-.51,3.3A2,2,0,0,0,51,95Z"></path>
+                  <path class="left-knuckle" d="M20.29,108a4.15,4.15,0,0,0,1-.11l1.42-.31c1.73-.38,2.7-1.7,2.16-2.93s-2.38-1.93-4.12-1.54l-1.42.31c-1.73.39-2.7,1.7-2.16,2.93A3.35,3.35,0,0,0,20.29,108Z"></path>
+                  <path class="starburst starburst--1" d="M45,32.09a2.11,2.11,0,0,0,1.67.91,2.07,2.07,0,0,0,1.62-.85,3.62,3.62,0,0,0,.05-4.24L37,12.91a2,2,0,0,0-3.29-.06,3.62,3.62,0,0,0-.05,4.24Z"></path>
+                  <path class="starburst starburst--2" d="M73.5,25A2.57,2.57,0,0,0,76,22.37V2.63a2.5,2.5,0,1,0-5,0V22.37A2.57,2.57,0,0,0,73.5,25Z"></path>
+                  <path class="starburst starburst--3" d="M89.88,33a2.87,2.87,0,0,0,2-.84L107.15,17a2.89,2.89,0,0,0,0-4.09,2.86,2.86,0,0,0-4,0l0,0L87.85,28.05a2.91,2.91,0,0,0,0,4.09A2.88,2.88,0,0,0,89.88,33Z"></path>
+                  <path class="starburst starburst--4" d="M117.49,49h-20a2.5,2.5,0,1,0,0,5h20a2.5,2.5,0,1,0,0-5Z"></path>
+                </svg>
+              </div>
+
+              <p>
+                Gothamist is non-profit local news. We rely on reader support. We need YOU so we can keep telling New York stories.
+              </p>
+
+              <a href="https://pledge3.wnyc.org/donate/gothamist/onestep?utm_medium=partnersite&amp;utm_source=gothamist&amp;utm_campaign=articledonationask" target="_blank" rel="noopener" class="o-button  ">
+
+                Donate Now
+
+              </a>
+
+            </div>
+          </div>
+
+        </div>
+
+        <div class="c-article__ad">
+
+          <div id="ember821" class="ad-tag-wide ember-view">
+            <div id="ember822" class="ember-view">
+              <div class="break-margins">
+                <div class="flex-wrapper">
+                  <div id="ad_ember822"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="c-article__comments">
+
+          <div id="comments">
+            <div id="disqus_thread"><iframe id="dsq-app8777" name="dsq-app8777" allowtransparency="true" frameborder="0" scrolling="no" tabindex="0" title="Disqus" width="100%"
+                src="https://disqus.com/embed/comments/?base=default&amp;f=gothamist&amp;t_i=708749&amp;t_u=https%3A%2F%2Fgothamist-client.wagtail.nypr.digital%2Fnews%2Fnypd-judge-cop-who-used-fatal-chokehold-on-eric-garner-should-be-fired&amp;t_d=%20%20%0A%20%20%20%20%20%20%20%20%20%20NYPD%20Judge%3A%20Cop%20Who%20Used%20Fatal%20Chokehold%20On%20Eric%20Garner%20Should%20Be%20Fired%0A%20%20%20%20%20%20%20%20%0A&amp;t_t=%20%20%0A%20%20%20%20%20%20%20%20%20%20NYPD%20Judge%3A%20Cop%20Who%20Used%20Fatal%20Chokehold%20On%20Eric%20Garner%20Should%20Be%20Fired%0A%20%20%20%20%20%20%20%20%0A&amp;s_o=default#version=bfb040859409d53aa5df08eec7df2a14"
+                style="width: 1px !important; min-width: 100% !important; border: none !important; overflow: hidden !important; height: 5848px !important;" horizontalscrolling="no" verticalscrolling="no" class="ui-droppable"></iframe><iframe
+                id="dsq-app8780" name="dsq-app8780" allowtransparency="true" frameborder="0" scrolling="no" tabindex="0" title="Disqus" sandbox="allow-forms allow-popups allow-same-origin allow-scripts" width="100%"
+                src="//tempest.services.disqus.com/ads-iframe/google/?position=bottom&amp;shortname=gothamist&amp;experiment=network_default&amp;variant=fallthrough&amp;service=dynamic&amp;anchorColor=%239c1e14&amp;colorScheme=light&amp;sourceUrl=https%3A%2F%2Fgothamist-client.wagtail.nypr.digital%2Fnews%2Fnypd-judge-cop-who-used-fatal-chokehold-on-eric-garner-should-be-fired&amp;typeface=sans-serif&amp;canonicalUrl=&amp;disqus_version=e4a6e2f"
+                style="width: 1px !important; min-width: 100% !important; border: none !important; overflow: hidden !important; height: 0px !important;" class="ui-droppable"></iframe></div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="l-container l-container--14col">
+  </div>
+</article>`;
+
+module.exports = {
+ARTICLE_BODY,
+}
